ff5f50003da1dcf2980a0d4f29ae4a25c4b8f35b
Bipin Y. <67560915+ybipin6283@users.noreply.github.com>
2025-09-10 19:34:07 +0530

    Update variable.tf

--- terraform /cosmos DB/variable.tf
+++ terraform /cosmos DB/variable.tf
@@ -1,1 +1,12 @@
+variable "reource_group_name" {
+  description = "The name of the resource group where the storage account is located."
+  type        = string
+  default     = "rf-resourcegrp"
+}
+variable "location" {
+  description = "The Azure region where the resources will be created."
+  type        = string
+  default     = "West Europe"
+
+}
 
