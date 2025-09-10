ebb1bbcdd6e90ab2a95e4ce2bdf3084fb6ba9f5d
Bipin Y. <67560915+ybipin6283@users.noreply.github.com>
2025-09-10 19:31:18 +0530

    Update main.tf

--- terraform /cosmos DB/main.tf
+++ terraform /cosmos DB/main.tf
@@ -1,1 +1,52 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.reource_group_name
+  location = var.location
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "tfex-cosmos-db-67414"
+  location            = var.location
+  resource_group_name = var.reource_group_name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  automatic_failover_enabled = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = "westus"
+    failover_priority = 0
+  }
+}
 
