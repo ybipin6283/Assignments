0e005bf4ba8368ac20100ec90b92775dd81aeeac
Bipin Y. <67560915+ybipin6283@users.noreply.github.com>
2025-09-10 19:32:01 +0530

    Update provider.tf

--- terraform /cosmos DB/provider.tf
+++ terraform /cosmos DB/provider.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.35.0"
+    }
+  }
+  required_version = ">=1.5.0"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  client_id       = "d1fe4589e2c"
+  client_secret   = "VHFMYtaCJaaXkXXe-6D~stS7JexGblQ" #value is the client secret
+  tenant_id       = "693bede74b"
+  subscription_id = "603ce8c4-f93"
+}
 
