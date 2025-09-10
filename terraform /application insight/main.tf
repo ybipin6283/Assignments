406287e5fd3539ae7848a02f165482c07fc4bab0
Bipin Y. <67560915+ybipin6283@users.noreply.github.com>
2025-09-10 19:28:24 +0530

    Update main.tf

--- terraform /application insight/main.tf
+++ terraform /application insight/main.tf
@@ -1,1 +1,13 @@
+resource "azurerm_resource_group" "example" {
+  name     = "tf-test"
+  location = "West Europe"
+}
 
+resource "azurerm_application_insights" "example" {
+  name                = "tf-test-appinsights"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  application_type    = "web"
+
+
+}
